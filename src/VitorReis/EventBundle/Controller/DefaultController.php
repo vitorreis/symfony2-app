64f6986b9e9882a8cf8b3ee7c6de2a8a595b7471
Vitor Reis <vitor.reis@rocket-internet.com.br>
2015-06-24 20:20:38 -0300

    playing with routing and responses type

--- src/VitorReis/EventBundle/Controller/DefaultController.php
+++ src/VitorReis/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace VitorReis\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+        //return $this->render('EventBundle:Default:index.html.twig', array('name' => $name));
+        $array = array(
+            'name' => $name,
+            'xpto' => 'It\'s a traaaaap!');
+
+        $json = json_encode($array);
+
+        $response = new Response($json);
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
